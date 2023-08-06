4286b22a6a1c4de74d90aec7fcaf434ef672ee2b
Sebastian Osthoff <sebastian.osthoff@gmail.com>
2023-08-06 14:00:54 -0700

    fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydub import AudioSegment
 import os
 import subprocess
 
@@ -6,10 +7,10 @@ def run_script(script_path, *args):
     cmd = ["python", script_path] + list(args)
     subprocess.run(cmd, check=True)
 
-def apply_mastering(input_path, output_path):
+def apply_mastering(input_audio, equalizer_settings):
     # Apply mastering techniques here
     # Example: Equalization
-    eq_audio = audio.equalize(bands=[(32, 31), (64, -12), (128, 6), (256, 0), (512, 6), (1024, 0), (2048, -6), (4096, -12)])
+    eq_audio = input_audio.equalize(bands=[(32, 31), (64, -12), (128, 6), (256, 0), (512, 6), (1024, 0), (2048, -6), (4096, -12)])
 
     # Example: Compression
     compressed_audio = eq_audio.compress_dynamic_range(threshold=-20.0, ratio=4.0)
@@ -20,8 +21,7 @@ def apply_mastering(input_path, output_path):
     # Example: Stereo Widening
     widened_audio = limited_audio.pan(-0.2) + limited_audio.pan(0.2)
 
-    # Export the mastered audio to the output path
-    widened_audio.export(output_path, format="mp3")
+    return widened_audio
 
 if __name__ == "__main__":
     input_video_folder_path = "videos"
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     run_script("reverb_enhancing.py", noise_reduced_output_folder, reverb_enhanced_output_folder)
 
     # Step 7: High-Pass and Low-Pass Filtering
-    run_script("high_lo_pass_filter.py", reverb_enhanced_output_folder, filtered_output_folder)
+    run_script("hi_lo_pass_filter.py", reverb_enhanced_output_folder, filtered_output_folder)
 
     # Step 8: Mastering
     for filename in os.listdir(filtered_output_folder):
@@ -67,7 +67,14 @@ if __name__ == "__main__":
             input_path = os.path.join(filtered_output_folder, filename)
             output_path = os.path.join(mastered_output_folder, filename)
 
-            apply_mastering(input_path, output_path)
+            # Load the audio file
+            audio = AudioSegment.from_mp3(input_path)
+
+            # Apply mastering to the audio
+            mastered_audio = apply_mastering(audio, equalizer_settings)
+
+            # Export the mastered audio to the output path
+            mastered_audio.export(output_path, format="mp3")
 
     # Step 9: Stitching
     run_script("stitch.py", mastered_output_folder, stitched_output_file)
--- reverb_enhancing.py
+++ reverb_enhancing.py
@@ -28,6 +28,6 @@ if __name__ == "__main__":
 
     # Adjust the reverb_duration (in milliseconds) and reverb_decay (0.0 to 1.0) parameters
     reverb_duration = 500
-    reverb_decay = 0.5
+    reverb_decay = 1
 
     apply_reverb_enhancements(input_folder_path, output_folder_path, reverb_duration, reverb_decay)
